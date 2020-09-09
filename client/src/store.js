e1ba6a8fc975a66cd89807ac3c54e28427cb113f
Brandon Peebles <peeblesbrandon@gmail.com>
2020-09-09 19:41:34 -0400

    remove redux dev tools from production

--- client/src/store.js
+++ client/src/store.js
@@ -3,7 +3,7 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initialState = {};
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancer = (process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null) || compose;
 const middleware = composeEnhancer(applyMiddleware(thunk));
 
 const store = createStore(
