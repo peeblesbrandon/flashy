962cf36e3af19de7913ebb912f3f5eae8f1d981e
Brandon Peebles <peeblesbrandon@gmail.com>
2020-09-11 20:35:28 -0400

    fix logout button

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -26,19 +26,19 @@ class Navbar extends Component {
         const { auth } = this.props;
         let LogoutButton;
         if (auth.isAuthenticated) {
-            LogoutButton = (
-                <button
-                    style={{
-                        width: "100px",
-                        borderRadius: "3px",
-                        letterSpacing: "1.5px",
-                        margin: "1rem 1rem 0 0"
-                    }}
-                    onClick={this.onLogoutClick}
-                    className="btn waves-effect right waves-light hoverable red accent-4 sidenav-close">
-                    Logout
-                </button>
-            );
+            // LogoutButton = (
+            //     <button
+            //         style={{
+            //             width: "100px",
+            //             borderRadius: "3px",
+            //             letterSpacing: "1.5px",
+            //             margin: "1rem 1rem 0 0"
+            //         }}
+            //         onClick={this.onLogoutClick}
+            //         className="btn waves-effect right waves-light hoverable red accent-4 sidenav-close">
+            //         Logout
+            //     </button>
+            // );
         }
         return (
             <div>
@@ -50,30 +50,37 @@ class Navbar extends Component {
                                 <i className="material-icons" style={{ color: "black" }}>whatshot</i> flashy
                         </Link>
                             {/* {LogoutButton} */}
-                            <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons md-36" style={{ color: "black" }}>menu</i></a>
-                            <ul className="right hide-on-med-and-down">
-                                <li><a href="#" className="black-text">Decks</a></li>
-                                <li><a href="#" className="black-text">Explore</a></li>
-                                <li><a href="#" className="black-text">Account</a></li>
-                                <li><a href="#" className="black-text">Logout</a></li>
-                            </ul>
+                            {auth.isAuthenticated &&
+                                <div>
+                                    <a href="#" data-target="slide-out" className="sidenav-trigger"><i className="material-icons md-36" style={{ color: "black" }}>menu</i></a>
+                                    <ul className="right hide-on-med-and-down">
+                                        <li><a href="#" className="black-text">Decks</a></li>
+                                        <li><a href="#" className="black-text">Explore</a></li>
+                                        <li><a href="#" className="black-text">Account</a></li>
+                                        <li><a href="#" className="black-text" onClick={this.onLogoutClick}>Logout</a></li>
+                                    </ul>
+                                </div>
+                            }
                         </div>
                     </nav>
                 </div>
                 <div>
-                    <ul id="slide-out" className="sidenav" >
-                        {/* <li><b className="flow-text right" style={{display: "block"}}>{auth.user.username}</b></li> */}
-                        <li style={{ paddingLeft: "1rem" }}>
-                            <h4>
-                                <span class="material-icons md-36 vertical-align-middle red-text text-darken-4">account_circle</span>
-                                <b style={{ padding: "0 0 1rem 1rem" }} className="vertical-align-middle padding-bottom-3 red-text text-darken-4">{auth.user.username}</b>
-                            </h4>
-                        </li>
-                        <li><a href="#" className="flow-text">Decks</a></li>
-                        <li><a href="#">Explore</a></li>
-                        <li><a href="#">Account</a></li>
-                        {/* <li>{LogoutButton}</li> */}
-                    </ul>
+                    {auth.isAuthenticated &&
+                        <ul id="slide-out" className="sidenav" >
+                            {/* <li><b className="flow-text right" style={{display: "block"}}>{auth.user.username}</b></li> */}
+                            <li style={{ paddingLeft: "1rem" }}>
+                                <h4>
+                                    <span class="material-icons md-36 vertical-align-middle red-text text-darken-4">account_circle</span>
+                                    <b style={{ padding: "0 0 1rem 1rem" }} className="vertical-align-middle padding-bottom-3 red-text text-darken-4">{auth.user.username}</b>
+                                </h4>
+                            </li>
+                            <li><a href="#" className="flow-text">Decks</a></li>
+                            <li><a href="#">Explore</a></li>
+                            <li><a href="#">Account</a></li>
+                            <li><a href="#" className="sidenav-close" onClick={this.onLogoutClick}>Logout</a></li>
+                            {/* <li>{LogoutButton}</li> */}
+                        </ul>
+                    }
                 </div>
             </div>
         );
