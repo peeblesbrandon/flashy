717ce04cbd4af492a34fd9ea62c41ab164f5ef86
Brandon Peebles <peeblesbrandon@gmail.com>
2020-09-26 10:08:17 -0400

    add demo login

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import PropTypes from 'prop-types';
+import { loginUser } from '../../actions/authActions';
 import { connect } from "react-redux";
 
 class Landing extends Component {
@@ -10,6 +11,25 @@ class Landing extends Component {
             this.props.history.push('/dashboard');
         }
     }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+            this.props.history.push('/dashboard'); // redirect user to dash after login
+            this.setState({
+                errors: {}
+            });
+        }
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+        // login for demo account
+        const userData = {
+            email: "demouser@gmail.com",
+            password: "LAQ@zqUDnq8mmt4"
+        };
+        this.props.loginUser(userData);
+    };
 
     render() {
         return (
@@ -37,10 +57,24 @@ class Landing extends Component {
                                 width: "140px",
                                 borderRadius: "3px",
                                 letterSpacing: "1.5px"
-                            }} className="btn btn-large btn-flat waves-effect white black-text">
+                            }} className="btn btn-large waves-effect white black-text">
                                 Login
                             </Link>
                         </div>
+                        <br />
+                        <form noValidate onSubmit={this.onSubmit}>
+                            <button
+                                style={{
+                                    width: "300px",
+                                    borderRadius: "3px",
+                                    letterSpacing: "1.5px",
+                                    marginTop: "1rem"
+                                }}
+                                type="submit"
+                                className="btn btn-large btn-flat waves-effect waves-light white grey-text">
+                                Demo login
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -49,6 +83,7 @@ class Landing extends Component {
 }
 
 Landing.propTypes = {
+    loginUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 };
@@ -60,7 +95,7 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    {}
+    { loginUser }
 )(withRouter(Landing));
 
 // export default Landing;
